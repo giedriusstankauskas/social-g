5cb16b21e6b99ab0b19c5ed302467c0029184fb2
giedriusstankauskas <46478940+giedriusstankauskas@users.noreply.github.com>
2022-07-19 09:06:38 +0300

    groups and posts added

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from . import forms
 from django.views.generic import CreateView
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 class SignUp(CreateView):
-    forms_class = forms.UserCreateForm
+    # model = User
+    form_class = forms.UserCreateForm
     success_url = reverse_lazy("login")
     template_name = "accounts/signup.html"
